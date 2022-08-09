a471411e4d01d5efb17f1ff4f91f101b1cb16e0b
Max <max.t.c.law@gmail.com>
2022-08-09 17:17:57 -0400

    connected Firebase, handle push of new lists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+// Import Styles
 import './App.css';
 
+// Import React Methods
+import { useState, useEffect } from 'react';
+
+// Import Firebase and Firebase Methods
+import firebase from './scripts/firebase';
+import { getDatabase, ref, onValue, push, remove, get } from 'firebase/database';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // Store current listKey for editing and display
+    const [listKey, setListKey] = useState('');
+
+    // Controlled input for list user
+    const [user, setUser] = useState(`Anonymous`)
+    const handleUserChange = (e) => {
+        setUser(e.target.value);
+    }
+
+    // Controlled input for list title
+    const [title, setTitle] = useState(`Anonymous's List`)
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value);
+    }
+
+    // Pushes a new list into Firebase with user, title, and time
+    const handleListInfoSubmit = (e) => {
+        e.preventDefault();
+        const database = getDatabase(firebase);
+        const dbRef = ref(database);
+
+        // Helper function to make 1-digit months and days two digits (e.g. January 0 => 01)
+        function twoDigits(num) {
+            return (num < 10 ? '0' : '') + num;
+        }
+
+        const  currentDate = new Date();
+        const currentTime = currentDate.getFullYear().toString()
+            + twoDigits(currentDate.getMonth() + 1)
+            + twoDigits(currentDate.getDate())
+            + currentDate.getHours()
+            + currentDate.getMinutes()
+            + currentDate.getSeconds()
+            + currentDate.getMilliseconds();
+        
+        const firebaseObj = push(dbRef, {
+            user: user,
+            title: title,
+            time: currentTime,
+            listItem: {}
+        });
+        
+        // Update the current working key for later use
+        setListKey(firebaseObj.key);
+    }
+
+    return (
+        <div className="App">
+            <h1>Public List</h1>
+
+            {/* Form for creating new list */}
+            <form onSubmit={handleListInfoSubmit}>
+                <label htmlFor="user">List Author: </label>
+                <input type="text" id="user" onChange={handleUserChange} value={user} required/>
+                
+                <label htmlFor="listTitle">List Title</label>
+                <input type="text" id="title" onChange={handleTitleChange} value={title} required/>
+
+                <button type="submit">Make my list!</button>
+            </form>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/scripts/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB_DOE7dkh6lwovVTEf10YWGL8I1ILzoYM",
+    authDomain: "public-list-6bbdf.firebaseapp.com",
+    projectId: "public-list-6bbdf",
+    storageBucket: "public-list-6bbdf.appspot.com",
+    messagingSenderId: "508553217150",
+    appId: "1:508553217150:web:bc38bc60709ae9db36c42f"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
