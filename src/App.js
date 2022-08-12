1d2002921d26ccd5bd02c6029049085d8601c2d5
Max <max.t.c.law@gmail.com>
2022-08-12 17:46:13 -0400

    allowed editing of list items

--- src/App.js
+++ src/App.js
@@ -22,12 +22,13 @@ const copyArrayObject = function (arr) {
 function App() {
 
     // STATES -------------------------------------------------------------------------------------
-    // Store current listKey for editing and display
-    const [listKey, setListKey] = useState('');
+    // All lists for display
     const [listsArray, setListsArray] = useState([]);
-
+    
+    // Current listKey and listItems
+    const [listKey, setListKey] = useState('');
     const [listItems, setListItems] = useState([]);
-    const sortSetListItems = function(listItems) {
+    const sortSetListItems = function (listItems) {
         const sortedListItems = objectToArray(listItems).sort((a, b) => {
             return (a[1].index < b[1].index ? -1 : 1)
         })
@@ -38,20 +39,20 @@ function App() {
     // CREATING NEW LISTS ---------------------------------
     // Controlled input for list user ✅
     const [user, setUser] = useState(`Anonymous`)
-    const handleUserChange = function(e) {
+    const handleUserChange = function (e) {
         setUser(e.target.value);
     }
 
     // Controlled input for list title ✅
     const [titleInput, setTitleInput] = useState(`Anonymous's List`)
-    const handleTitleChange = function(e) {
+    const handleTitleChange = function (e) {
         setTitleInput(e.target.value);
     }
 
     // CREATING LIST ITEMS --------------------------------
     // Controlled input for list item text ✅
     const [listItemTextInput, setListItemTextInput] = useState('');
-    const handleListItemTextChange = function(e) {
+    const handleListItemTextChange = function (e) {
         setListItemTextInput(e.target.value);
     }
 
@@ -59,12 +60,12 @@ function App() {
     // HANDLERS -----------------------------------------------------------------------------------
     // LIST BUTTON HANDLERS -------------------------------
     // Display list item for the given key✅
-    const handleDisplayList = function(key) {
+    const handleDisplayList = function (listKeyParam) {
 
-        setListKey(key);
+        setListKey(listKeyParam);
 
-        const listRef = ref(database, `/${key}`);
-        get(listRef).then((list) => {
+        const dbListRef = ref(database, `/${listKeyParam}`);
+        get(dbListRef).then((list) => {
             // Update listItems state if there are list items, otherwise keep state empty
             if ('listItems' in list.val()) {
                 sortSetListItems(list.val().listItems)
@@ -75,7 +76,7 @@ function App() {
     }
 
     // Pushes a new list into Firebase with user, title, and time ✅
-    const handleSubmitList = function(e) {
+    const handleSubmitList = function (e) {
         e.preventDefault();
 
         // Helper function to make 1-digit months and days two digits (e.g. January => 0 => 01)
@@ -102,11 +103,14 @@ function App() {
         handleDisplayList(firebaseObj.key);
     }
 
-    // Deletes a list from Firebase
-    const handleRemoveList = function (listKey) {
-        const dbListRef = ref(database, `/${listKey}`)
+    // Deletes a list from Firebase ✅
+    const handleRemoveList = function (listKeyParam) {
+        const dbListRef = ref(database, `/${listKeyParam}`)
 
         setListItems([]);
+        if (listKeyParam === listKey) {
+            setListKey('')
+        }
         remove(dbListRef);
     }
 
@@ -114,7 +118,7 @@ function App() {
 
     // LIST ITEMS BUTTON HANDLERS -------------------------
     // Pushes list item into the active list ✅
-    const handleSubmitListItem = function(e) {
+    const handleSubmitListItem = function (e) {
         e.preventDefault();
 
         // Ensure there is a list key
@@ -134,22 +138,20 @@ function App() {
                 })
             })
 
-
-
         }
 
     }
 
     // Removes individual list items, and shifts indices accordingly ✅
-    const handleRemoveListItem = function(listKey, listItemKey) {
-        const dbListItemsRef = ref(database, `/${listKey}/listItems`)
+    const handleRemoveListItem = function (listKeyParam, listItemKeyParam) {
+        const dbListItemsRef = ref(database, `/${listKeyParam}/listItems`)
 
         // Filter out keyed item, and shift indices
         const newListItemsArray = listItems.filter((item) => {
-            return item[0] !== listItemKey;
+            return item[0] !== listItemKeyParam;
         }).map((item, itemIndex) => {
             item[1].index = itemIndex;
-            return item
+            return item;
         });
 
         // Create object with updated item indices for Firebase
@@ -162,6 +164,50 @@ function App() {
         setListItems(newListItemsArray);
         set(dbListItemsRef, newListItemsObject)
     }
+
+
+    // EDIT LIST ITEM HANDLERS ----------------------------
+    // Handles state of item editing
+    const [editListItemKey, setEditListItemKey] = useState('');
+
+    // Controlled input for editing list item
+    const [editListItemTextInput, setEditListItemTextInput] = useState('')
+    const handleEditListItemTextInput = function (e) {
+        setEditListItemTextInput(e.target.value)
+    }
+
+    // Selecting list item to edit
+    const handleSelectEditListItem = function (listItemKeyParam, listItemTextParam) {
+        setEditListItemKey(listItemKeyParam);
+        setEditListItemTextInput(listItemTextParam);
+    }
+
+    // Submitting the edited list item
+    const handleSubmitEditListItem = function (e, listKeyParam, listItemKeyParam, editListItemTextParam) {
+        console.log(e);
+        e.preventDefault();
+
+
+        // Update listItems display state
+        const newListItemsArray = listItems;
+        newListItemsArray.map((item) => {
+            const itemKey = item[0];
+
+            if (itemKey === listItemKeyParam) {
+                item[1].text = editListItemTextParam;
+            }
+            return item;
+        })
+        setListItems(newListItemsArray)
+
+
+        // Update database and clear editing key
+        const dbListItemTextRef = ref(database, `/${listKeyParam}/listItems/${listItemKeyParam}/text`)
+        set(dbListItemTextRef, editListItemTextParam);
+        setEditListItemKey('')
+    }
+
+
 
 
     // USE EFFECT ---------------------------------------------------------------------------------
@@ -203,42 +249,70 @@ function App() {
             </form>
 
             {/* For Debugging # */}
-            <p>Your current key is {listKey}</p>
+            <p>Your current list key is: <strong>{listKey ? listKey : 'no list selected'}</strong></p>
+            <p>Your current list item key is: <strong>{editListItemKey ? editListItemKey : 'no list item selected'}</strong></p>
 
             {/* Div for displaying all of the existing lists */}
             <div>
                 {
                     listsArray.length ? <div>
-                        <h2>There are {listsArray.length} lists</h2> 
+                        <p>There {listsArray.length === 1 ? 'is' : 'are'} currently <strong>{listsArray.length} public list{listsArray.length === 1 ? '' : 's'}</strong></p> 
                         <ul> {
                         listsArray.map((list) => {
+                            
+                            const listObjectKey = list[0];
+                            const listTitle = list[1].title;
+                            const listUser = list[1].user;
+
                             return (
                                 <li key={list[0]}>
-                                    <button onClick={() => { handleDisplayList(list[0]) }}>view this list</button>
-                                    <button onClick={() => { handleRemoveList(list[0]) }}>X</button>
-                                    <h3>{ list[1].title }</h3>
-                                    <h4>{ list[1].user }</h4>
+                                    <button onClick={() => { handleDisplayList(listObjectKey) }}>view this list</button>
+                                    <button onClick={() => { handleRemoveList(listObjectKey) }}>X</button>
+                                    <h3>{ listTitle }</h3>
+                                    <h4>{ listUser }</h4>
+                                    <p>Number of items in this list: {list[1].listItems? Object.keys(list[1].listItems).length : 0}</p>
                                 </li>
                             )
                         })
                     } </ul> 
-                    </div> : <p>There are currently no public lists...</p>
+                    </div> : <p>There are currently <strong>0 public lists</strong></p>
                 }
             </div>
             
             {/* Div for displaying everything in the current list */}
             <div>
                 {
-                    listItems.length ? <ul> {
+                    listItems.length ? <ul>
+                        <p>Below here is your selected list, which has <strong>{listItems.length} items</strong></p>
+                        {
                         listItems.map((item) => {
-                            return (
-                                <li key={item[0]}>
-                                    <button onClick={() => { handleRemoveListItem(listKey, item[0]) }}>X</button>
-                                    <p>{item[1].text}</p>
-                                </li>
-                            )
+
+                            const itemKey = item[0];
+                            const itemText = item[1].text;
+
+                            if (editListItemKey === itemKey) {
+                                return (
+                                    <li key={itemKey}>
+                                        <button onClick={() => { handleRemoveListItem(listKey, itemKey) }}>X</button>
+                                        <form onSubmit={(e) => { handleSubmitEditListItem(e, listKey, itemKey, editListItemTextInput) }}>
+                                            <label htmlFor="editListItem"></label>
+                                            <input type="text" name="editListItem" id="editListItem" onChange={handleEditListItemTextInput} value={editListItemTextInput} />
+                                            <button>Submit my edit</button>
+                                        </form>
+                                        <p>original item text: {itemText}</p>
+                                    </li>
+                                )          
+                            } else {
+                                return (
+                                    <li key={itemKey}>
+                                        <button onClick={() => { handleSelectEditListItem(itemKey, itemText)}}>Edit</button>
+                                        <button onClick={() => { handleRemoveListItem(listKey, itemKey) }}>X</button>
+                                        <p>{itemText}</p>
+                                    </li>
+                                )                                
+                            }
                         })
-                    } </ul> : <p>There's currently nothing in your selected list!</p>
+                    } </ul> : <p>There's currently <strong>nothing</strong> in your selected list!</p>
                 }
             </div>
             
